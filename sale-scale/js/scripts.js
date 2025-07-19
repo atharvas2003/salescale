979872a1de91d9beba725b7b5557c0807ebfa975
atharvas2003 <85014221+atharvas2003@users.noreply.github.com>
2025-07-20 04:27:47 +0530

    Add files via upload

---
+++ sale-scale/js/scripts.js
@@ -0,0 +1,9 @@
+// Smooth scroll for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      const target = document.querySelector(link.getAttribute('href'));
+      target.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+  
